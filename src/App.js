c9fd25d97ed34f5adf236fa7d5a9a996fd20e00c
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-21 18:09:51 +0600

    parallax issue fixed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 
 import original from "react95/dist/themes/original";
 import { ThemeProvider } from 'styled-components';
+import { Rnd } from 'react-rnd';
 
 import GlobalStyle from './theme/globalStyles';
 
 import Projects from './Components/Projects/Projects';
+import Avatar from './Components/Avatar/Avatar';
 
 
 
@@ -17,6 +19,7 @@ function App() {
       <ThemeProvider theme={original}>
         <Projects />
       </ThemeProvider>
+      <Avatar />
     </>
   );
 }
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,13 +1,18 @@
 // globalStyles.js
 import { createGlobalStyle } from 'styled-components';
- 
+
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background: teal;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
-  }
+	body, 
+	html, 
+	#root {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		background: teal;
+		font-family: Open-Sans, Helvetica, Sans-Serif;
+	}
 `;
- 
+
 export default GlobalStyle;
+
