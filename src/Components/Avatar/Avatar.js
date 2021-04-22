cab1695aad2da7735bdf502d2443fc91a13c41a8
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-23 01:01:10 +0600

    character placing

--- src/Components/Avatar/Avatar.js
+++ src/Components/Avatar/Avatar.js
@@ -9,26 +9,31 @@ const calc = (x, y) => {
 	return [x - window.innerWidth / 2, y - window.innerHeight / 2]
 };
 
-const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
-const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
-const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
-const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
+const trans1 = (x, y) => `translate3d(${x / 5010}px,${y / 5010}px,0)`
+const trans2 = (x, y) => `translate3d(${x / 5040 - 25}px,${y / 5040 - 70}px,0)`
+const trans3 = (x, y) => `translate3d(${x / 5030 - 20 }px,${y / 5030 - 85}px,0)`
+const trans4 = (x, y) => `translate3d(${x / 5025 - 120}px,${y / 5025 - 22}px,0)`
 
 
 function Avatar() {
-	const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
+	const [props, set] = useSpring(() => ({ xy: [ 6000, 6000], config: { mass: 10, tension: 550, friction: 140 } }))
 	return (
 		<div className="avatarHolder" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
-			<Rnd>
 				<div class="container" >
 					<animated.div class="card1" style={{ transform: props.xy.interpolate(trans1) }} />
 					<animated.div class="card2" style={{ transform: props.xy.interpolate(trans2) }} />
 					<animated.div class="card3" style={{ transform: props.xy.interpolate(trans3) }} />
 					<animated.div class="card4" style={{ transform: props.xy.interpolate(trans4) }} />
 				</div>
-			</Rnd>
 		</div>
 	)
 }
 
 export default Avatar;
+
+
+/*const trans1 = (x, y) => `translate3d(${x / 50}px,${y / 50}px,0)`
+const trans2 = (x, y) => `translate3d(${x / 40 - 25}px,${y / 40 -70}px,0)`
+const trans3 = (x, y) => `translate3d(${x / 30 - 20 }px,${y / 30 - 85}px,0)`
+const trans4 = (x, y) => `translate3d(${x / 25 - 120}px,${y / 25 - 22}px,0)`
+*/
